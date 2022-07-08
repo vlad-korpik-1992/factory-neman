b55b7ff8122406f03c8e89c5d038183f89095f64
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2022-07-08 13:01:34 +0300

    added pages about cart catalog jobs our-stores

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+    $('.stores__body__link').click(function (e) {
+        e.preventDefault();
+        let elem = e.target;
+        console.log(elem);
+		let id = '1' + elem.getAttribute('id');
+		$('.stores__body__info').removeClass('stores__body__info--active');
+		jQuery("#"+id).addClass('stores__body__info--active');
+	});
+    $('.stores__head__link--ourstore').click(function (e) {
+        e.preventDefault();
+        $('.stores__head__link--ourstore').addClass('stores__head__link--active');
+        $('.stores__head__link--partners').removeClass('stores__head__link--active');
+        $('.stores__body--ourstore').addClass('stores__body--active');
+        $('.stores__body--partners').removeClass('stores__body--active');
+	});
+    $('.stores__head__link--partners').click(function (e) {
+        e.preventDefault();
+        $('.stores__head__link--partners').addClass('stores__head__link--active');
+        $('.stores__head__link--ourstore').removeClass('stores__head__link--active');
+        $('.stores__body--partners').addClass('stores__body--active');
+        $('.stores__body--ourstore').removeClass('stores__body--active');
+	});
     $('.scrollto a').on('click', function scroll(e) {
 		e.preventDefault();
 		let href = $(this).attr('href');
@@ -55,3 +77,45 @@ $(document).ready(function() {
         }
     }
 })();
+const rangeInput = document.querySelectorAll(".range-input input");
+const priceInput = document.querySelectorAll(".price-input__items input");
+
+let priceGap = 50;
+
+priceInput.forEach(input=>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(priceInput[0].value),
+        maxVal = parseInt(priceInput[1].value);
+        
+        if((maxVal - minVal >= priceGap) && maxVal <= 1000 ){
+            if(e.target.className === "input-min price__number price__number--left"){
+                rangeInput[0].value = minVal;
+                console.log(minVal);
+                $('.filter__progress').css({"left": (minVal / rangeInput[0].max) * 100 + "%"});
+            }else{
+                rangeInput[1].value = maxVal;
+                $('.filter__progress').css({"right": 100 - (maxVal / rangeInput[1].max) * 100 + "%"});
+            }
+        }
+    });
+})
+
+rangeInput.forEach(input=>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        
+        if(maxVal - minVal < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap;
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            $('.filter__progress').css({"left": (minVal / rangeInput[0].max) * 100 + "%"});
+            $('.filter__progress').css({"right": 100 - (maxVal / rangeInput[1].max) * 100 + "%"});
+        }
+    });
+})
